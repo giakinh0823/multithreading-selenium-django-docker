f38707710e69bf8eb1fa7eb4ead01ee05d52a701
Gia Kinh <giakinh2000@gmail.com>
2021-08-11 16:04:52 +0700

    oke

--- cralThread/settings.py
+++ cralThread/settings.py
@@ -153,6 +153,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
